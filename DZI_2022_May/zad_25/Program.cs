f06cfbbb6c2958e3f243fffa9cecfc7550208baf
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-05-17 16:46:20 +0300

    Added zad_25

---
+++ DZI_2022_May/zad_25/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace zad_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<int, int> collection = new Dictionary<int, int>();
+                int n = int.Parse(Console.ReadLine());
+
+                for(int i = 0; i < n; i++)
+                {
+                    int number = int.Parse(Console.ReadLine());
+
+                    if(!collection.ContainsKey(number))
+                    {
+                        collection.Add(number, 0);
+                    }                   
+                    collection[number]++;
+                }
+
+                foreach(KeyValuePair<int, int> kvp in collection)
+                {
+                    Console.WriteLine($"число: {kvp.Key}, брой: {kvp.Value}");
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Моля, въведете число!");
+            }
+        }
+    }
+}
