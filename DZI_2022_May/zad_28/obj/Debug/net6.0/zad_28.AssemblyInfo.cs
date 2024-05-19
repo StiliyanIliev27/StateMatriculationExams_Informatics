acf5ae9a1855d2d4d25215a8d15fa4851d688743
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-05-19 22:49:51 +0300

    Changed some files

--- DZI_2022_May/zad_28/obj/Debug/net6.0/zad_28.AssemblyInfo.cs
+++ DZI_2022_May/zad_28/obj/Debug/net6.0/zad_28.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zad_28")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2772e1ba0a46b4bc765e28bf1818b33f77ced101")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b710a8424564e30ebac41bdf60d7f6e89143f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("zad_28")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zad_28")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
